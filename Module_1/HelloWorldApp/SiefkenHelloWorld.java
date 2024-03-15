49d60e33388b32311942f41f1f6c3bea4fe1b43f
clay.siefken@nelnet.net <dcsiefken@my365.bellevue.edu>
2024-03-15 16:11:59 -0500

    added solution for module 1

---
+++ Module_1/HelloWorldApp/SiefkenHelloWorld.java
@@ -0,0 +1,23 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by A. Student 2021
+    Additional modifications by Clay Siefken 2024
+*/ 
+public class SiefkenHelloWorld {
+
+    /**
+     * Method with two arguments, which are used to print the user's first and last name.
+     * @param firstName String 
+     * @param lastName String
+     * @return prints the results to the console window.
+     */
+    public static void helloWorld(String firstName, String lastName) {
+        System.out.printf("Hello World from %s %s\r\n", firstName, lastName);
+    } // end helloWorld
+
+    public static void main(String[] args) {
+        helloWorld("Clay", "Siefken"); // Call the helloWorld() method with the values "Clay" and "Siefken."
+    } // end main
+} // end SiefkenHelloWorld
